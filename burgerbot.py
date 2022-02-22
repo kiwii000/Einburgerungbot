facbe015e156bd89877a4f595d76a5753af4007b
Kiwii00 <74303450+kiwii000@users.noreply.github.com>
2022-02-22 13:31:34 +0100

    Update burgerbot.py

--- burgerbot.py
+++ burgerbot.py
@@ -15,7 +15,7 @@ from telegram.ext import CommandHandler, Updater
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.update import Update
 
-url = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=120686&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,327346,122243,327348,122252,329742,122260,329745,122262,329748,122254,329751,122271,327278,122273,327274,122277,327276,330436,122280,327294,122282,327290,122284,327292,327539,122291,327270,122285,327266,122286,327264,122296,327268,150230,329760,122301,327282,122297,327286,122294,327284,122312,329763,122314,329775,122304,327330,122311,327334,122309,327332,122281,327352,122279,329772,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,122257,327322,122208,327298,122226,327300&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120686%2F'
+url = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister=324180&anliegen[]=318998&herkunft=1'
 register_prefix = 'https://service.berlin.de'
 
 @dataclass
@@ -60,7 +60,7 @@ class Bot:
   def __start(self, update: Update, context: CallbackContext) -> None:
     self.__add_chat(update.message.chat_id)
     logging.info(f'got new user with id {update.message.chat_id}')
-    update.message.reply_text('Welcome to BurgerBot. When there will be slot - you will receive notification. To stop it - just type /stop')
+    update.message.reply_text('Welcome to EinburgerungBot. When there will be slot - you will receive notification. To stop it - just type /stop')
 
 
   def __stop(self, update: Update, context: CallbackContext) -> None:
